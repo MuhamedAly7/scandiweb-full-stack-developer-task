2340ba38aa64a5351da5c511c3d8760aa3878e5e
MuhamedAly7 <muhammedaly021@gmail.com>
2024-12-29 18:30:52 +0200

    edit on host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 3000,
     proxy: {
       "/graphql": {
-        target: "https://fullstack-scandiweb-mohamedhassan.web1337.net",
+        target: "http://fullstack-scandiweb-mohamedhassan.web1337.net/graphql",
         changeOrigin: true,
         secure: false,
       },
