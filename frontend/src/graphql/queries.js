20ef7a7744fc6ee02ce038661e9fc8fd3630a7b8
MuhamedAly7 <muhammedaly021@gmail.com>
2024-12-29 23:35:27 +0200

    edit the GET_PRODUCTS query

--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -36,11 +36,34 @@ export const GET_CATEGORIES = gql`
 `;
 
 export const GET_PRODUCTS = gql`
-    query ($category: String!) {
-        products(category: $category) {
-            ${productFields}
+  query ($category: String!) {
+    products(category: $category) {
+      id
+      name
+      inStock
+      gallery
+      description
+      brand
+      prices {
+        amount
+        currency {
+          label
+          symbol
         }
+      }
+      category
+      attributes {
+        id
+        name
+        type
+        items {
+          id
+          value
+          display_value
+        }
+      }
     }
+  }
 `;
 
 export const GET_SINGLE_PRODUCT = gql`
