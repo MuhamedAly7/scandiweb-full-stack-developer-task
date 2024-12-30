a29becb6eb7fe8e4e4dceb13b3aa6ef71484f639
MuhamedAly7 <muhammedaly021@gmail.com>
2024-12-30 02:26:25 +0200

    edite on pdp forgotten2

--- frontend/src/components/ProductAttribute.jsx
+++ frontend/src/components/ProductAttribute.jsx
@@ -7,7 +7,7 @@ function ProductAttribute({ attribute, productDetail, setProductDetail }) {
       {attribute.items.map((item) => {
         return (
           <button
-            data-testid={`product-attribute${attribute.name}-${item.value
+            data-testid={`product-attribute-${attribute.name}-${item.value
               .replace(/\s+/g, "-")
               .toLowerCase()}`}
             className={`${attribute.type} ${item.selected && "selected"}`}
