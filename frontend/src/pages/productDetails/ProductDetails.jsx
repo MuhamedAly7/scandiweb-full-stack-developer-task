d2593e867e232cb1fb00b6fb203817757586ef6d
MuhamedAly7 <muhammedaly021@gmail.com>
2025-01-24 16:11:48 +0200

    removing loading from product details page

--- frontend/src/pages/productDetails/ProductDetails.jsx
+++ frontend/src/pages/productDetails/ProductDetails.jsx
@@ -45,7 +45,7 @@ const ProductDetails = () => {
     }
   }, [data]);
 
-  if (loading) return <h1>Loading...</h1>;
+  if (loading) return <h1> </h1>;
   if (error) return <h1>Error: {error.message}</h1>;
 
   // console.log(productDetail);
