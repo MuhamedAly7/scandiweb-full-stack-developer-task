1fc61456916cf6c1ca96817caa945f129441c2c5
MuhamedAly7 <muhammedaly021@gmail.com>
2025-01-26 23:52:37 +0200

    solve problems in the cart overlay for repeated products

--- frontend/src/features/slices/cart-slice.js
+++ frontend/src/features/slices/cart-slice.js
@@ -24,31 +24,41 @@ export const cartSlice = createSlice({
       );
 
       if (identicalProduct) {
-        // Increment quantity for the identical product
         identicalProduct.quantity += 1;
       } else {
-        // Add as a new product if no identical product exists
         state.push(product);
       }
     },
     updateProductInCart: (state, action) => {
       const { updatedProduct, productIndex } = action.payload;
 
-      // Check if the updated product is identical to any other product in the cart
-      const identicalProductIndex = state.findIndex(
-        (item, index) =>
-          index !== productIndex &&
-          item.id === updatedProduct.id &&
-          areAttributesIdentical(updatedProduct.attributes, item.attributes)
-      );
+      if (state[productIndex].quantity > 1) {
+        state[productIndex].quantity -= 1;
 
-      if (identicalProductIndex !== -1) {
-        // If an identical product is found, increment its quantity and remove the current product
-        state[identicalProductIndex].quantity += state[productIndex].quantity;
-        state.splice(productIndex, 1);
+        const identicalProduct = state.find(
+          (item) =>
+            item.id === updatedProduct.id &&
+            areAttributesIdentical(updatedProduct.attributes, item.attributes)
+        );
+
+        if (identicalProduct) {
+          identicalProduct.quantity += 1;
+        } else {
+          state.push({ ...updatedProduct, quantity: 1 });
+        }
       } else {
-        // If no identical product is found, simply update the product at the current index
-        state[productIndex] = updatedProduct;
+        const identicalProduct = state.find(
+          (item) =>
+            item.id === updatedProduct.id &&
+            areAttributesIdentical(updatedProduct.attributes, item.attributes)
+        );
+
+        if (identicalProduct) {
+          identicalProduct.quantity += 1;
+          state.splice(productIndex, 1);
+        } else {
+          state[productIndex] = { ...updatedProduct };
+        }
       }
     },
     removeFromCart: (state, action) => {
@@ -58,10 +68,8 @@ export const cartSlice = createSlice({
         const product = state[productIndex];
 
         if (product.quantity > 1) {
-          // Decrement quantity if it's greater than 1
           product.quantity -= 1;
         } else {
-          // Remove the product from the cart if quantity equals 1
           state.splice(productIndex, 1);
         }
       }
